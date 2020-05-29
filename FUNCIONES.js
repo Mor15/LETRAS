58761ace3363c02a049c1bcd095ff9e0b8f432ec
ArathMor <51805378+cloudsG15@users.noreply.github.com>
2020-05-29 14:20:50 -0500

    Add files via upload

---
+++ FUNCIONES.js
@@ -0,0 +1,25 @@
+
+function cuervojaja() {
+
+document.getElementById("compre").onclick=colorjaja;
+
+}
+
+
+
+
+
+
+function colorjaja() {
+
+alert("espere mientras el producto es enviado");
+
+
+
+}
+
+
+
+window.onload=cuervojaja;
+
+
